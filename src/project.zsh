49ae65c55b09a1d1ac06613ca1c06c45977d9eae
Luka Vandervelden <lukc@upyum.com>
2014-03-12 16:27:27 +0100

    Forgotten project.zsh update.

--- src/project.zsh
+++ src/project.zsh
@@ -5,7 +5,7 @@ type[mpkgmk]="binary"
 sources[mpkgmk]="assemble.c build.c configuration.c download.c extraction.c main.c modules.c recipe.c workdir.c"
 cflags[mpkgmk]='-DLIBDIR=\"$(LIBDIR)/mpkgmk\" -DSHAREDIR=\"$(SHAREDIR)/mpkgmk\"'
 ldflags[mpkgmk]="-ldl -lyaml -lmpkgmk"
-depends[mpkgmk]="error.h include/mpkgmk.h include/mpkgmk_private.h"
+depends[mpkgmk]="error.h path_max.h include/mpkgmk.h include/mpkgmk_private.h"
 
 subdirs=(modules include)
 
